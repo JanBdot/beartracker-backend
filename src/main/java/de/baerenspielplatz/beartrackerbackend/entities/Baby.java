8a5b9d13dcfdbf2ed4410980303395195a8e9da6
JanBdot <janthebaer@gmail.com>
2021-02-24 12:06:31 +0100

    only create baby entitiy

--- src/main/java/de/baerenspielplatz/beartrackerbackend/entities/Baby.java
+++ src/main/java/de/baerenspielplatz/beartrackerbackend/entities/Baby.java
@@ -14,7 +14,7 @@ import java.util.UUID;
 public class Baby {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private UUID id;
 
     @Column(nullable = false)
